a78a8ed6948f914730973673fc4bc82ebcc59eca
ppgdf0714 <ppgdf0714@163.com>
2020-12-10 22:16:37 +0800

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+      0 => '9999999-dev',
+    ),
+    'reference' => '6f7ddc2638f1d3f0ee204c640de413c394c08b57',
+    'name' => 'phppgadmin/phppgadmin',
+  ),
+  'versions' => 
+  array (
+    'phppgadmin/phppgadmin' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '6f7ddc2638f1d3f0ee204c640de413c394c08b57',
+    ),
+  ),
+);
